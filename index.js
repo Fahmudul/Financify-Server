80e89df05e3c9f3e9ee9ffce6123bef66f7bd542
Fahmudul Hassan <fahmudul234@gmail.com>
2024-07-18 21:01:09 +0600

    completed basic functionalities including cashIn cashout transfer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
@@ -7,15 +7,17 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://Financify:${process.env.MONGODB_PASS}@cluster0.nkzn5jr.mongodb.net/?appName=Cluster0`;
-app.get("/", (req, res) => {
-  res.send("Hello World!dd");
-});
 const client = new MongoClient(uri, {
-  serverApi: {
+  serverApi: { 
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
@@ -33,29 +35,30 @@ const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
   console.log("token", authHeader);
   if (!authHeader) {
+    console.log("no token");
     return res.status(401).send({ message: "Unauthorized access" });
   }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    }
-    req.decoded = decoded;
-    console.log("decoded", decoded);
-    console.log("verified");
-    next();
-  });
+  next();
 };
 async function run() {
   try {
     const db = client.db("Financify");
     const acceptedUserCollection = db.collection("users");
-
+    const transactionCollection = db.collection("transactions");
+    const cashInRequestCollection = db.collection("cash-in-requests");
     const requestedUserCollection = db.collection("requested-users");
 
     // Registered visitor to be User or Agent
     app.post("/request-user", async (req, res) => {
       const requestedUser = req.body;
+      const phone = req.body.phone;
+      const accessToken = jwt.sign(
+        requestedUser,
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "10d",
+        }
+      );
       // Checking is this user already requested
       const alreadyRequested = await requestedUserCollection.findOne({
         email: requestedUser.email,
@@ -77,12 +80,257 @@ async function run() {
         });
       }
       const result = await requestedUserCollection.insertOne(requestedUser);
+      const registeredUserExist = await requestedUserCollection.findOne({
+        phone,
+      });
+      // console.log("from",registeredUser);
+      const registeredUser = {
+        email: registeredUserExist.email,
+        phone: registeredUserExist.phone,
+        role: registeredUserExist.appliedFor,
+        currentRole: registeredUserExist.currentRole,
+      };
       return res.send({
         success: true,
         message: "Request sent wait for approval",
         result,
+        registeredUser,
+        accessToken,
       });
     });
+    // Get all Transactions (Admin only)
+    app.get("/transactions", async (req, res) => {
+      const result = await transactionCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Get transaction by phone number
+    app.get("/transaction-history", async (req, res) => {
+      const phone = req.query.phone;
+      // console.log(phone);
+      const result1 = await transactionCollection
+        .find({ receiverNumber: phone })
+        .toArray();
+      const result2 = await transactionCollection
+        .find({ senderNumber: phone })
+        .toArray();
+      // console.log("result from 120", result);
+      const result = [...result1, ...result2];
+      res.send(result);
+    });
+    // Send money
+    app.patch("/send-money", async (req, res) => {
+      const transferDetails = req.body;
+      const { receiverNumber, senderNumber, amount } = req.body;
+      // console.log("from line 110 transferDetails ", transferDetails);
+      const SenderDetails = await acceptedUserCollection.findOne({
+        phone: transferDetails.senderNumber,
+      });
+      // console.log("from line 114 sender", SenderDetails);
+      if (SenderDetails) {
+        // checking if sender typed correct pin
+        const mathcedPassword = await bcrypt.compare(
+          transferDetails.pin,
+          SenderDetails.hashedPassword
+        );
+        // console.log("from line 121 pass", mathcedPassword);
+        if (!mathcedPassword) {
+          return res.send({ success: false, message: "Wrong pin" });
+        }
+        // checking if sender has enough balance
+        else if (SenderDetails.accountBalance < transferDetails.amount) {
+          // console.log("from line 127", SenderDetails.accountBalance);
+          return res.send({ success: false, message: "Insufficient balance" });
+        }
+        // Checking if receiver is valid
+        const ReceiverDetails = await acceptedUserCollection.findOne({
+          phone: transferDetails.receiverNumber,
+        });
+        // console.log("from line 134 rec", ReceiverDetails);
+        if (!ReceiverDetails) {
+          return res.send({ success: false, message: "Receiver not found" });
+        }
+        const transferedDetails = await transactionCollection.insertOne({
+          receiverNumber,
+          senderNumber,
+          transactionType: "Send Money",
+          amount: parseInt(transferDetails.amount),
+          date: new Date().toLocaleString("en-US"),
+        });
+        // console.log("reciver amount", typeof ReceiverDetails.accountBalance);
+        const deductedBalanceOfSender =
+          parseInt(SenderDetails.accountBalance) -
+          parseInt(transferDetails.amount);
+
+        const deductedBalanceOfReceiver =
+          parseInt(ReceiverDetails.accountBalance) +
+          parseInt(transferDetails.amount);
+        // console.log(deductedBalanceOfReceiver, deductedBalanceOfSender);
+        const deductedFromSender = await acceptedUserCollection.updateOne(
+          { phone: transferDetails.senderNumber },
+          {
+            $set: {
+              accountBalance: parseInt(deductedBalanceOfSender),
+            },
+          }
+        );
+
+        const addedToReceiver = await acceptedUserCollection.updateOne(
+          { phone: transferDetails.receiverNumber },
+          {
+            $set: {
+              accountBalance: parseInt(deductedBalanceOfReceiver),
+            },
+          }
+        );
+
+        res.send({ deductedFromSender, addedToReceiver, transferedDetails });
+      }
+    });
+
+    // add Cash in request to database
+    app.post("/cash-in", async (req, res) => {
+      const cashInDetails = req.body;
+      // console.log(typeof cashInDetails.requestedAmount);
+      // Check if agent exists
+      const agentExist = await acceptedUserCollection.findOne({
+        phone: cashInDetails.agentNumber,
+      });
+      if (!agentExist) {
+        return res.send({ success: false, message: "Agent not found" });
+      }
+      const result = await cashInRequestCollection.insertOne(cashInDetails);
+      res.send({ success: true, result });
+    });
+
+    // Get all cash in/out requests
+    app.get("/cash-in-requests-list", async (req, res) => {
+      const result = await cashInRequestCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Accept Cashin request
+    app.patch("/accept-cashin-request", async (req, res) => {
+      const cashInDetails = req.body;
+      const { phone, amount, agentNumber } = cashInDetails;
+      // console.log(cashInDetails);
+      const userDetails = await acceptedUserCollection.findOne({
+        phone: cashInDetails.phone,
+      });
+      // Checking if agent has enough balance
+      const agentExist = await acceptedUserCollection.findOne({
+        phone: cashInDetails.agentNumber,
+      });
+      if (agentExist.accountBalance < cashInDetails.amount) {
+        return res.send({ success: false, message: "Insufficient balance" });
+      }
+      // Add amount to user account
+      const addedBalanceOfuser =
+        parseInt(userDetails.accountBalance) + parseInt(cashInDetails.amount);
+      const deductedBalanceOfAgent =
+        parseInt(agentExist.accountBalance) - parseInt(cashInDetails.amount);
+      const addedToUser = await acceptedUserCollection.updateOne(
+        { phone: cashInDetails.phone },
+        {
+          $set: {
+            accountBalance: parseInt(addedBalanceOfuser),
+          },
+        }
+      );
+
+      const deductedFromAgent = await acceptedUserCollection.updateOne(
+        {
+          phone: cashInDetails.agentNumber,
+        },
+        {
+          $set: {
+            accountBalance: parseInt(deductedBalanceOfAgent),
+          },
+        }
+      );
+      // Delete cash in request
+      const deleted = await cashInRequestCollection.deleteOne({
+        senderNumber: cashInDetails.phone,
+      });
+      // Add to transaction history
+      const result = await transactionCollection.insertOne({
+        receiverNumber: cashInDetails.phone,
+        senderNumber: cashInDetails.agentNumber,
+        amount: parseInt(cashInDetails.amount),
+        transactionType: "Cash In",
+        date: new Date().toLocaleString("en-US"),
+      });
+      res.send({ addedToUser, deductedFromAgent, deleted });
+    });
+
+    // Add cash-out requests to database
+    app.post("/cash-out", async (req, res) => {
+      const cashOutDetails = req.body;
+      const senderDetails = await acceptedUserCollection.findOne({
+        phone: cashOutDetails.senderNumber,
+      });
+      // Checking if sender typed invalid pin
+      const mathcedPassword = await bcrypt.compare(
+        cashOutDetails.pin,
+        senderDetails.hashedPassword
+      );
+      if (!mathcedPassword) {
+        return res.send({ success: false, message: "Wrong pin" });
+      }
+
+      // Check if agent exists
+      const agentExist = await acceptedUserCollection.findOne({
+        phone: cashOutDetails.agentNumber,
+      });
+      if (!agentExist) {
+        return res.send({ success: false, message: "Agent not found" });
+      }
+      // CashoutDetails without pin
+      delete cashOutDetails.pin;
+
+      // Calculate total amount of both sender and agent balance
+      const fee = parseInt(cashOutDetails.requestedAmount) * 0.015;
+      const totalAmount = parseInt(cashOutDetails.requestedAmount) + fee;
+      const deductedBalanceOfSender =
+        senderDetails.accountBalance - totalAmount;
+      const addedBalanceOfAgent = agentExist.accountBalance + fee;
+      const deductedFromSender = await acceptedUserCollection.updateOne(
+        {
+          phone: cashOutDetails.senderNumber,
+        },
+        {
+          $set: {
+            accountBalance: parseInt(deductedBalanceOfSender),
+          },
+        }
+      );
+
+      const addedToAgent = await acceptedUserCollection.updateOne(
+        {
+          phone: cashOutDetails.agentNumber,
+        },
+        {
+          $set: {
+            accountBalance: parseInt(addedBalanceOfAgent),
+          },
+        }
+      );
+      // Add to transaction history
+      const result2 = await transactionCollection.insertOne({
+        receiverNumber: cashOutDetails.agentNumber,
+        senderNumber: cashOutDetails.senderNumber,
+        amount: parseInt(cashOutDetails.requestedAmount),
+        transactionType: "Cash out",
+        date: new Date().toLocaleString("en-US"),
+      });
+      res.send({
+        success: true,
+
+        message: "Cash out request sent. Wait for agent approval",
+      });
+    });
+
+    // Accept Cashout request
 
     // Log in request
     app.post("/login-number-email", async (req, res) => {
@@ -93,8 +341,6 @@ async function run() {
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "10d",
       });
-      // console.log(accessToken);
-      // res.send({ accessToken });
       // Regular expression for detecting a phone number with exactly 11 digits
       const phoneRegex = /^\d{11}$/;
       let userRequestWithType;
@@ -165,10 +411,12 @@ async function run() {
     });
 
     // Get one user
-    app.get("/user-phone", verifyJWT, async (req, res) => {
+    app.get("/user-phone", async (req, res) => {
       const phone = req.query.phone;
+      console.log("from line 414", phone);
       const user = await acceptedUserCollection.findOne({ phone: phone });
       res.send(user);
+      console.log("from line 417", user);
     });
 
     // Get all  users
@@ -183,7 +431,7 @@ async function run() {
     // Accept Visitor Request
     app.patch("/accept-visitor-request", async (req, res) => {
       // console.log("req.body", req.query);
-      console.log("route hitting");
+      // console.log("route hitting");
       const phone = req.query.phone;
       const action = req.query.action;
       if (action === "approved") {
@@ -225,8 +473,7 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+   
   }
 }
 run().catch(console.dir);
